87eb35d44bf766caf0aa11de428d6481143ad154
Smade War <smadewar@gmail.com>
2026-01-29 21:08:31 -0500

    initia commit

---
+++ src/main/java/com/Reactive_Programming/Spring_boot_Reactive_programming/SpringBootReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.Reactive_Programming.Spring_boot_Reactive_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Reactive_Programming/Spring_boot_Reactive_programming/SpringBootReactiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Reactive_Programming.Spring_boot_Reactive_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
